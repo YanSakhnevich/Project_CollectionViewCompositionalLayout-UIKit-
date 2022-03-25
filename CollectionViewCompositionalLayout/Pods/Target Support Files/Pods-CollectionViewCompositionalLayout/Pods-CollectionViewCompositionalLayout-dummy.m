14df992f1d5f5dc6035e860397aee4de9dcfe34d
YanSakhnevich <yansakhnevich@gmail.com>
2022-03-25 12:41:52 +1100

    project created, pods installed

---
+++ CollectionViewCompositionalLayout/Pods/Target Support Files/Pods-CollectionViewCompositionalLayout/Pods-CollectionViewCompositionalLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CollectionViewCompositionalLayout : NSObject
+@end
+@implementation PodsDummy_Pods_CollectionViewCompositionalLayout
+@end
